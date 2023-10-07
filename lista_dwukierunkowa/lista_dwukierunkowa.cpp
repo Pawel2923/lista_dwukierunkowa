cc79c88301420a83b1beccf8a00996282af32e64
Paweł <pawelporemba123@gmail.com>
2023-10-07 14:58:54 +0200

    Dodanie struktury ListElement i klasy List

--- lista_dwukierunkowa/lista_dwukierunkowa.cpp
+++ lista_dwukierunkowa/lista_dwukierunkowa.cpp
@@ -1,20 +1,32 @@
-﻿// lista_dwukierunkowa.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
 
-#include <iostream>
+struct ListElement {
+    ListElement* prev;
+    ListElement* next;
+    std::string data;
+    int key;
+};
+
+class List {
+private:
+    ListElement* first, * last;
+    unsigned counter;
+public:
+    List() {
+        first = last = NULL;
+        counter = 0;
+    }
+    ~List() {
+        ListElement* element;
+        while (first) {
+            element = first->next;
+            delete first;
+            first = element;
+        }
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    List list1;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
