d02eeb05a3cc5a894ff7973feb0374828947e548
Paweł <pawelporemba123@gmail.com>
2023-10-07 15:35:30 +0200

    Dodanie metod unshift i printList

--- lista_dwukierunkowa/lista_dwukierunkowa.cpp
+++ lista_dwukierunkowa/lista_dwukierunkowa.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 struct ListElement {
     ListElement* prev;
     ListElement* next;
-    std::string data;
     int key;
 };
 
@@ -12,10 +13,7 @@ private:
     ListElement* first, * last;
     unsigned counter;
 public:
-    List() {
-        first = last = NULL;
-        counter = 0;
-    }
+    List() : first(NULL), last(NULL), counter(0) {}
     ~List() {
         ListElement* element;
         while (first) {
@@ -24,9 +22,53 @@ public:
             first = element;
         }
     }
+    
+    unsigned unshift(ListElement* element) {
+        element->next = first;
+        element->prev = NULL;
+
+        if (first) {
+            first->prev = element;
+        }
+        first = element;
+        if (!last) {
+            last = first;
+        }
+        
+        return ++counter;
+    }
+
+    void printList() {
+        if (!first) {
+            cout << "\nLista jest pusta\n";
+            return;
+        }
+
+        ListElement* element;
+        element = first;
+
+        cout << "----------------Wszystkie elementy listy----------------\n";
+        while (element)
+        {
+            cout << element->key << " ";
+            element = element->next;
+        }
+        cout << "\n--------------------------------------------------------\n";
+    }
 };
 
 int main()
 {
     List list1;
+    ListElement* element;
+
+    // Dodanie 5 elementów na początek listy
+    for (unsigned i = 0; i < 5; i++)
+    {
+        element = new ListElement;
+        element->key = i + 1;
+        list1.unshift(element);
+    }
+
+    list1.printList();
 }
